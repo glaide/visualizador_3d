b3fd0e52e2705a92d339b30321a04e6c6d44f073
Glaide <42775870+glaide@users.noreply.github.com>
2019-10-24 22:29:54 -0300

    Update wireframe.c
    atualizei por aqui porque não esta dando pelo meu computador, por algum motivo

--- wireframe.c
+++ wireframe.c
@@ -1,5 +1,27 @@
 // arquivo principal
 #include "datatypes.h"
-#include "graphics.h"
+#include <stdio.h>
+#include <stdlib.h>
+//#include "graphics.h"
 #include "objread.h"
-#include "perspect.h"
+//#include "perspect.h"
+#include <SDL/SDL.h> 
+#include <SDL/SDL_image.h> 
+#include <SDL/SDL_timer.h> 
+
+int main(int argv, char** argv)
+{
+    FILE **obj;
+// returns zero on success else non-zero 
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) 
+    { 
+        printf("error initializing SDL: %s\n", SDL_GetError()); 
+    } 
+    SDL_Window* win = SDL_CreateWindow("GAME",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 1000, 1000, 0); 
+    while (1); 
+  
+
+    //le o arquivo e verifica se foi aberto corretamente
+    //leObj(obj,argc, argv);
+    return 0; 
+}
