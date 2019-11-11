d8f692c1f1e623e2dfc7873c985475275b54ff74
glaide <glaide.lara@gmail.com>
2019-11-11 14:52:49 -0300

    arrumei uma parte da leitura, fazer a projeçao

--- perspect.c
+++ perspect.c
@@ -7,12 +7,20 @@
 
 // funções de cálculo de perspectiva
 
-void persp(convert *doisD, vert *vet, FILE *obj, int tam)
+void persp(convert *doisD, vert *vet, FILE *obj, int tamV)
 {
     /*
     xp = xc + zc × ( (xv - xc) ÷ (zv + zc) )
     yp = yc + zc × ( (yv - yc) ÷ (zv + zc) )
     */
+
+//    vai ate o tamanho do vetor de vertices
+
+    for (int i=0; i < tamV; i++)
+    {
+        doisD[i].x=
+        doisD[i].y=
+    }
    
    
 }
