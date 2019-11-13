f67aeff146a0b39af8a6fcf98b40195eac1bec9b
glaide <glaide.lara@gmail.com>
2019-11-13 02:13:34 -0200

    coloquei pouca coisa

--- perspect.c
+++ perspect.c
@@ -15,11 +15,16 @@ void persp(convert *doisD, vert *vet, FILE *obj, int tamV)
     */
 
 //    vai ate o tamanho do vetor de vertices
+        
+    //verificar esses valores
+    float xc=0;
+    float yc=0;
+    float zc=100;
 
     for (int i=0; i < tamV; i++)
     {
-        doisD[i].x=
-        doisD[i].y=
+        doisD[i].x= xc + zc *( (vet[i].x - xc) / (vet[i].z + zc) );
+        doisD[i].y= yc + zc *( (vet[i].y - yc) / (vet[i].z + zc) )
     }
    
    
